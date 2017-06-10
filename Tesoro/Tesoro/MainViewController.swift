310f5ca91a7fce3571d952220311304cfae989e4
CuGi <ji3g4m6zo6@msn.com>
2017-06-10 13:12:07 +0800

    Firebase 初步完成

---
+++ Tesoro/Tesoro/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  Tesoro
+//
+//  Created by CuGi on 2017/6/10.
+//  Copyright © 2017年 Cu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TSGFirebaseManager.share.userCountListener { (error, userCount) in
+            print("人數變動\(userCount)")
+        }
+        
+        TSGFirebaseManager.share.gameStatusListener { (error, isGameStart) in
+            if (isGameStart) {
+                print("遊戲開始")
+            }else{
+                print("遊戲結束")
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ Tesoro/Tesoro/TSGFirebaseManager.swift
@@ -0,0 +1,87 @@
+//
+//  TSGFirebaseManager.swift
+//  Tesoro
+//
+//  Created by CuGi on 2017/6/10.
+//  Copyright © 2017年 Cu. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class TSGFirebaseManager: NSObject {
+    var ref: DatabaseReference!
+    var isServer: Bool!
+    var userCount: Int = 0 {
+        didSet {
+            self.ref.child("game_state").updateChildValues(["start": false])
+            if userCount >= 2 {
+                //server
+                
+                //init
+                isServer = true
+                self.ref.child("game_state").child("user").updateChildValues(["user_count": 1])
+                self.ref.child("game_state").child("user").child("server").updateChildValues(["isOnline" : true])
+                self.ref.child("game_state").child("user").child("client").updateChildValues(["isOnline" : false])
+                self.ref.child("game_state").updateChildValues(["questions": ["book", "photo"]])
+            }else{
+                //client
+                isServer = false
+                self.ref.child("game_state").child("user").updateChildValues(["user_count": userCount + 1])
+                self.ref.child("game_state").child("user").child("client").updateChildValues(["isOnline" : true])
+            }
+            print(userCount)
+        }
+    }
+    
+    typealias CountCompletionHandler = (_ error:NSError?, _ userCount: Int) -> Void
+    typealias GameStatusCompletionHandler = (_ error:NSError?, _ gmaeStart: Bool) -> Void
+    
+    static let share : TSGFirebaseManager = {
+        let instance = TSGFirebaseManager()
+        return instance
+    }()
+    
+    override init() {
+        self.ref = Database.database().reference()
+    }
+    
+    func countContro(){
+        ref.child("game_state").child("user").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let userCount = value?["user_count"] as? Int ?? 0
+            self.userCount = userCount
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    
+    func userCountListener(comp: @escaping CountCompletionHandler) {
+        ref.child("game_state").observe(DataEventType.childChanged, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            guard let userCount = value?["user_count"] as? Int else {
+                return
+            }
+            if userCount > 0 {
+                comp(nil, userCount)
+            }
+        })
+    }
+    
+    func gameStatusListener(comp: @escaping GameStatusCompletionHandler) {
+        ref.child("game_state").observe(DataEventType.childChanged, with: { (snapshot) in
+            guard let isStart = snapshot.value as? Bool else {
+                return
+            }
+            comp(nil, isStart)
+        })
+    }
+    
+    func gmaeStart() {
+        if isServer {
+            self.ref.child("game_state").updateChildValues(["start": true])
+        }
+    }
+    
+}
