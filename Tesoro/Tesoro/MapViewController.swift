af4ed14df177fa0f3826413ba0d4c71636465c2b
CuGi <ji3g4m6zo6@msn.com>
2017-06-10 16:16:19 +0800

    Fix StoryBoard.

---
+++ Tesoro/Tesoro/MapViewController.swift
@@ -0,0 +1,81 @@
+//
+//  MapViewController.swift
+//  Tesoro
+//
+//  Created by Lucy on 2017/6/10.
+//  Copyright © 2017年 Cu. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var ScrollMapView: UIScrollView!
+    @IBOutlet weak var enemyImageView: UIImageView!
+    @IBOutlet weak var crownImageView: UIImageView!
+    @IBOutlet weak var crownProgressView: UIView!
+    @IBOutlet weak var enemyProgressView: UIView!
+    @IBOutlet weak var userProgressDotsStackView: UIStackView!
+    @IBOutlet weak var enemyProgressDotsStackView: UIStackView!
+    
+    let mapImageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewController()
+        
+        let testView = UIView()
+        testView.backgroundColor = .red
+        testView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        testView.layer.cornerRadius = testView.frame.width / 2
+        testView.layer.masksToBounds = true
+//        self.view.addSubview(testView)
+        
+    }
+    
+    func setupViewController() {
+        
+        //load map imageview
+        mapImageView.image = #imageLiteral(resourceName: "treasue-map")
+        mapImageView.frame = CGRect(x: 0, y: 0,
+                                    width: ScrollMapView.frame.width * 1.5,
+                                    height: ScrollMapView.frame.height)
+        mapImageView.contentMode = .scaleAspectFit
+        ScrollMapView.addSubview(mapImageView)
+        
+        //scrollView
+        ScrollMapView.contentSize = mapImageView.frame.size
+        print(ScrollMapView.contentSize)
+        
+        //vibrate image view
+        crownImageView.vibrate(amplitude: 10, duration: 0.5)
+        enemyImageView.vibrate(amplitude: 10, duration: 0.5)
+        
+        //progress stack view
+        userProgressDotsStackView.spacing = userProgressDotsStackView.frame.width / 8
+        userProgressDotsStackView.translatesAutoresizingMaskIntoConstraints = false
+        userProgressDotsStackView.centerXAnchor.constraint(equalTo: crownProgressView.centerXAnchor).isActive = true
+        userProgressDotsStackView.centerYAnchor.constraint(equalTo: crownProgressView.centerYAnchor).isActive = true
+        userProgressDotsStackView.widthAnchor.constraint(equalToConstant: crownProgressView.frame.width - 20).isActive = true
+        userProgressDotsStackView.heightAnchor.constraint(equalToConstant: crownProgressView.frame.height + 10).isActive = true
+        for i in 0...4 {
+            userProgressDotsStackView.subviews[i].layer.cornerRadius = userProgressDotsStackView.frame.height / 4
+
+        }
+        enemyProgressDotsStackView.spacing = enemyProgressDotsStackView.frame.width / 8
+        enemyProgressDotsStackView.translatesAutoresizingMaskIntoConstraints = false
+        enemyProgressDotsStackView.centerXAnchor.constraint(equalTo: enemyProgressView.centerXAnchor).isActive = true
+        enemyProgressDotsStackView.centerYAnchor.constraint(equalTo: enemyProgressView.centerYAnchor).isActive = true
+        enemyProgressDotsStackView.widthAnchor.constraint(equalToConstant: enemyProgressView.frame.width - 20).isActive = true
+        enemyProgressDotsStackView.heightAnchor.constraint(equalToConstant: enemyProgressView.frame.height + 10).isActive = true
+        for i in 0...4 {
+            enemyProgressDotsStackView.subviews[i].layer.cornerRadius = enemyProgressDotsStackView.frame.height / 4
+            
+        }
+
+        
+        
+    }
+
+}
