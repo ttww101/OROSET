9296e925141afd235bfa95739182be49b804c127
CuGi <ji3g4m6zo6@msn.com>
2017-06-10 10:21:16 +0800

    Install SwiftyJSON.

---
+++ Tesoro/Tesoro/TSGVManager.swift
@@ -0,0 +1,14 @@
+//
+//  TSGVManager.swift
+//  Tesoro
+//
+//  Created by CuGi on 2017/6/10.
+//  Copyright © 2017年 Cu. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class TSGVManager: NSObject {
+
+}
